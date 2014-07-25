8b8011b174d23f9afd68f3751f61209d63b71859
liubo <popoyliu@hotmail.com>
2014-07-25 13:42:00 +0800

    增加登陆、设备初始化VO代码

---
+++ student_app/AClassRoom/src/cn/com/incito/classroom/vo/LoginRes2Vo.java
@@ -0,0 +1,42 @@
+package cn.com.incito.classroom.vo;
+
+/**
+ * Created by JOHN on 2014/7/25.
+ */
+public class LoginRes2Vo {
+    private String name;
+    private String number;
+    private String sex;
+    private String islogin;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getIslogin() {
+        return islogin;
+    }
+
+    public void setIslogin(String islogin) {
+        this.islogin = islogin;
+    }
+}
