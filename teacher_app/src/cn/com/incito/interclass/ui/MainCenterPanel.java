e4d7f3e824fd236fc8da56563d024f02ca97d118
liushiping <6423669@qq.com>
2014-07-27 15:37:45 +0800

    增加资源文件和代码

---
+++ teacher_app/src/cn/com/incito/interclass/ui/MainCenterPanel.java
@@ -0,0 +1,175 @@
+package cn.com.incito.interclass.ui;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Stroke;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import cn.com.incito.interclass.po.Group;
+
+public class MainCenterPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6316121486627261595L;
+
+	public MainCenterPanel() {
+//		this.setSize(878, 620);
+		this.setLayout(null);
+		this.setOpaque(false);
+
+		int x = 20;
+		for (int i = 0; i < 8; i++) {
+			JPanel pnlTable1 = createTable(String.valueOf(i + 1), null);
+			pnlTable1.setBounds(20, x, 836, 139);
+			add(pnlTable1);
+			x += 150;
+		}
+	}
+
+	private JPanel createTable(String tableNumber,Group group) {
+		JPanel pnlTable = new JPanel() {
+			private static final long serialVersionUID = 1778895558158714379L;
+
+			@Override
+			protected void paintComponent(Graphics g) {
+				Image iconUser = new ImageIcon("images/main/bg_list.png")
+						.getImage();
+				g.drawImage(iconUser, 0, 0, this.getWidth(), this.getHeight(),
+						this);
+			}
+		};
+		pnlTable.setLayout(null);
+		String url = String.format("images/main/pic_%s.png", tableNumber);
+		Icon icon = new ImageIcon(url);
+		JLabel number = new JLabel("课桌号", icon, JLabel.LEFT);
+		number.setHorizontalTextPosition(JLabel.LEFT);
+		pnlTable.add(number);
+		number.setBounds(15, 5, 70, 30);
+
+		JLabel lblName = new JLabel("哇哈哈" + tableNumber);
+		pnlTable.add(lblName);
+		lblName.setBounds(80, 5, 80,30);
+		
+		ImageIcon imgLogo = new ImageIcon("images/main/ico_pad_connection.png");
+		JLabel lblLogo = new JLabel();
+		lblLogo.setIcon(imgLogo);
+		pnlTable.add(lblLogo);
+		lblLogo.setBounds(40, 60, imgLogo.getIconWidth(), imgLogo.getIconHeight());
+
+		JLabel lblLine1 = getLine();
+		pnlTable.add(lblLine1);
+		lblLine1.setBounds(160, 10, 2,120);
+		
+		ImageIcon imgPadOnline = new ImageIcon("images/main/ico_pad_connection.png");
+		ImageIcon imgPadOffline = new ImageIcon("images/main/ico_pad_disconnect.png");
+
+		JLabel lblPad1 = new JLabel();
+		lblPad1.setIcon(imgPadOffline);
+		pnlTable.add(lblPad1);
+		lblPad1.setBounds(200, 30, imgPadOffline.getIconWidth(), imgPadOffline.getIconHeight());
+
+		JLabel lblPad2 = new JLabel();
+		lblPad2.setIcon(imgPadOffline);
+		pnlTable.add(lblPad2);
+		lblPad2.setBounds(270, 30, imgPadOffline.getIconWidth(), imgPadOffline.getIconHeight());
+
+		JLabel lblPad3 = new JLabel();
+		lblPad3.setIcon(imgPadOffline);
+		pnlTable.add(lblPad3);
+		lblPad3.setBounds(200, 80, imgPadOffline.getIconWidth(), imgPadOffline.getIconHeight());
+
+		JLabel lblPad4 = new JLabel();
+		lblPad4.setIcon(imgPadOffline);
+		pnlTable.add(lblPad4);
+		lblPad4.setBounds(270, 80, imgPadOffline.getIconWidth(), imgPadOffline.getIconHeight());
+
+		JLabel lblLine2 = getLine();
+		pnlTable.add(lblLine2);
+		lblLine2.setBounds(367, 10, 2,120);
+		
+		//list1
+		JLabel lblName1 = getNameLabel("", false);
+		pnlTable.add(lblName1);
+		lblName1.setBounds(400, 12, 90, 24);
+		JLabel lblName2 = getNameLabel("", false);
+		pnlTable.add(lblName2);
+		lblName2.setBounds(400, 42, 90, 24);
+		JLabel lblName3 = getNameLabel("", false);
+		pnlTable.add(lblName3);
+		lblName3.setBounds(400, 72, 90, 24);
+		JLabel lblName4 = getNameLabel("", false);
+		pnlTable.add(lblName4);
+		lblName4.setBounds(400, 102, 90, 24);
+		//list2
+		JLabel lblName21 = getNameLabel("", false);
+		pnlTable.add(lblName21);
+		lblName21.setBounds(510, 12, 90, 24);
+		JLabel lblName22 = getNameLabel("", false);
+		pnlTable.add(lblName22);
+		lblName22.setBounds(510, 42, 90, 24);
+		JLabel lblName23 = getNameLabel("", false);
+		pnlTable.add(lblName23);
+		lblName23.setBounds(510, 72, 90, 24);
+		JLabel lblName24 = getNameLabel("", false);
+		pnlTable.add(lblName24);
+		lblName24.setBounds(510, 102, 90, 24);
+		//list3
+		JLabel lblName31 = getNameLabel("", false);
+		pnlTable.add(lblName31);
+		lblName31.setBounds(620, 12, 90, 24);
+		JLabel lblName32 = getNameLabel("", false);
+		pnlTable.add(lblName32);
+		lblName32.setBounds(620, 42, 90, 24);
+		JLabel lblName33 = getNameLabel("", false);
+		pnlTable.add(lblName33);
+		lblName33.setBounds(620, 72, 90, 24);
+		JLabel lblName34 = getNameLabel("", false);
+		pnlTable.add(lblName34);
+		lblName34.setBounds(620, 102, 90, 24);
+		
+		return pnlTable;
+	}
+
+	private JLabel getLine() {
+		return new JLabel() {
+			private static final long serialVersionUID = 2679733728559406364L;
+			@Override
+			public void paint(Graphics g) {
+				Graphics2D g2d = (Graphics2D) g;
+				Stroke stroke = g2d.getStroke();
+				Color color = g2d.getColor();
+				g2d.setStroke(new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER));
+				g2d.setColor(new Color(Integer.parseInt("e1e1e1", 16)));
+				g2d.drawLine(0, 0, 0, this.getHeight());
+				g2d.setStroke(stroke);
+				g2d.setColor(color);
+				this.paintComponents(g2d);
+			}
+		};
+	}
+
+	private JLabel getNameLabel(String name,final boolean isOnline){
+		JLabel lblName = new JLabel(name, JLabel.CENTER);
+		lblName.setOpaque(true);
+		lblName.setBackground(new Color(Integer.parseInt("e1e1e1", 16)));
+		lblName.setForeground(new Color(Integer.parseInt("000000", 16)));
+		if(isOnline){
+			lblName.setBackground(new Color(Integer.parseInt("5ec996", 16)));
+			lblName.setForeground(new Color(Integer.parseInt("ffffff", 16)));
+		}
+		return lblName;
+	}
+	
+	public static void main(String[] args) {
+		MainFrame.getInstance().setVisible(true);
+	}
+}
--- teacher_app/src/cn/com/incito/interclass/ui/MainFrame.java
+++ teacher_app/src/cn/com/incito/interclass/ui/MainFrame.java
@@ -1,20 +1,39 @@
 package cn.com.incito.interclass.ui;
-
+/**
+ * 互动课堂主界面
+ * @author 刘世平
+ */
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+import cn.com.incito.server.api.Application;
 import cn.com.incito.server.core.CoreSocket;
 
-public class MainFrame extends JFrame implements MouseListener{
+public class MainFrame extends MouseAdapter{
 
-	private static final long serialVersionUID = 199999999L;
+	private JFrame frame= new JFrame();
+	private Boolean isDragged;
+	private Point loc, tmp;
 	private JButton btnMin, btnClose;
+	private JLabel lblBackground;
+	private JPanel contentPane;
+	private JButton btnBegin;
+	private Application app = Application.getInstance();
 	private static MainFrame instance;
 	
 	public static MainFrame getInstance() {
@@ -24,23 +43,35 @@ public class MainFrame extends JFrame implements MouseListener{
 		return instance;
 	}
 
-	private MainFrame() {
-		//启动通讯线程
-		CoreSocket.getInstance().start();
-		initFrame();
+	public void setVisible(boolean show){
+		frame.setVisible(show);
 	}
 	
-
-	public void initFrame() {
-		setSize(460,290);
-		setDefaultCloseOperation(3);
-		setLocationRelativeTo(null);//设置窗体中间位置
-		setLayout(null);//绝对布局
-		setUndecorated(true);//去除窗体
-		setBackground(Color.WHITE);//窗体透明
+	private MainFrame(){
+		//启动通讯线程
+		CoreSocket.getInstance().start();
+		showLoginUI();
+		setDragable();
+	}
+	
+	//显示登陆界面
+	private void showLoginUI(){
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
+		frame.setSize(1004, 748);
+		frame.setDefaultCloseOperation(3);
+		frame.setLocationRelativeTo(null);//设置窗体中间位置
+		frame.setLayout(null);//绝对布局
+		frame.setUndecorated(true);//去除窗体
+		frame.setAlwaysOnTop(true); //设置界面悬浮
+		
+		
+		////////////////////////top部分////////////////////////
 		JPanel top = new JPanel();
-		top.setSize(460,30);
+		top.setSize(1004,73);
 		top.setLayout(null);
 		top.setOpaque(false);
 		
@@ -51,7 +82,7 @@ public class MainFrame extends JFrame implements MouseListener{
 		ImageIcon imgMin = new ImageIcon("images/login/4.png");
 		btnMin.setIcon(imgMin);//设置图片
 		top.add(btnMin);//添加按钮
-		btnMin.setBounds(385, 9, imgMin.getIconWidth(), imgMin.getIconHeight());
+		btnMin.setBounds(938, -1, imgMin.getIconWidth(), imgMin.getIconHeight());
 		btnMin.addMouseListener(this);
 		
 		//关闭按钮
@@ -61,36 +92,233 @@ public class MainFrame extends JFrame implements MouseListener{
 		ImageIcon imgMax = new ImageIcon("images/login/7.png");
 		btnClose.setIcon(imgMax);//设置图片
 		top.add(btnClose);//添加按钮
-		btnClose.setBounds(412, 9, imgMax.getIconWidth(), imgMax.getIconHeight());
+		btnClose.setBounds(965, -1, imgMax.getIconWidth(), imgMax.getIconHeight());
 		btnClose.addMouseListener(this);
-		add(top);
+		
+		//logo
+		JLabel lblLogo = new JLabel();
+		ImageIcon imgLogo = new ImageIcon("images/main/logo.png");
+		lblLogo.setIcon(imgLogo);
+		top.add(lblLogo);
+		lblLogo.setBounds(13, 23, imgLogo.getIconWidth(), imgLogo.getIconHeight());
+		
+		//menu
+		JLabel btnHome = new JLabel();
+		ImageIcon imgHome = new ImageIcon("images/main/ico_home_foucs.png");
+		btnHome.setIcon(imgHome);
+		top.add(btnHome);
+		btnHome.setBounds(150, 40, imgLogo.getIconWidth(), imgLogo.getIconHeight());
+				
+		contentPane.add(top);
 		
 		
+		////////////////////////left部分////////////////////////
+		JPanel left = new JPanel();
+		left.setSize(127,930);
+		left.setLayout(null);
+		left.setOpaque(false);
 		
-	}
+		//准备菜单
+		Icon icon = new ImageIcon("images/main/ico_menu_1.png");
+        JLabel lblStatus = new JLabel(" 准备", icon, JLabel.CENTER);
+		left.add(lblStatus);
+		lblStatus.setBounds(0, 0, 127, 56);
+		
+		//用户信息
+		JPanel pnlUser = new JPanel(){
+			private static final long serialVersionUID = 1778895558158714379L;
 
+			@Override
+			protected void paintComponent(Graphics g) {
+				Image iconUser = new ImageIcon("images/main/bg_user.png").getImage();
+		        g.drawImage(iconUser, 0, 0, this.getWidth(), this.getHeight(), this);  
+		    }
+			
+		};
+		pnlUser.setLayout(null);
+		String user = String.format("您好,%s!", app.getTeacher().getName());
+		JLabel lblTeacher = new JLabel(user,JLabel.CENTER);
+		pnlUser.add(lblTeacher);
+		lblTeacher.setBounds(0, 70, 127, 20);
+		left.add(pnlUser);
+		pnlUser.setBounds(0, 55, 127, 95);
+		
+		JLabel lblCopyRight = new JLabel("Copy Right © 2014",JLabel.CENTER);
+		lblCopyRight.setBounds(0, 605, 127, 20);
+		left.add(lblCopyRight);
+		JLabel lblCompany1 = new JLabel("四川灵动",JLabel.CENTER);
+		lblCompany1.setBounds(0, 630, 127, 20);
+		left.add(lblCompany1);
+		JLabel lblCompany2 = new JLabel("信息技术有限公司",JLabel.CENTER);
+		lblCompany2.setBounds(0, 645, 127, 20);
+		left.add(lblCompany2);
+		
+		left.setBounds(0, 73, 127,930);
+		contentPane.add(left);
+		
+		/////////////////////center部分////////////////////////
+		MainCenterPanel centerPanel = new MainCenterPanel();
+		centerPanel.setOpaque(true);
+		centerPanel.setBackground(Color.WHITE);
+		JScrollPane scrollPane = new JScrollPane(centerPanel);
+		scrollPane.setBorder(null);
+		scrollPane.setBounds(127, 75, 878, 618);
+		centerPanel.setPreferredSize(new Dimension(scrollPane.getWidth() - 50, scrollPane.getHeight() * 2));
+		contentPane.add(scrollPane);
+		centerPanel.revalidate();
+		
+		////////////////////////bottom部分////////////////////////
+		JPanel bottom = new JPanel();
+		bottom.setSize(878,48);
+		bottom.setLayout(null);
+		bottom.setOpaque(false);
+		bottom.setBounds(127, 694, 878,54);
+		
+		JPanel pnlClass = new JPanel(){
+			private static final long serialVersionUID = 5365972834168199801L;
+			@Override
+			protected void paintComponent(Graphics g) {
+				Image iconClass = new ImageIcon("images/main/bg_input_kc.png").getImage();
+		        g.drawImage(iconClass, 0, 0, this.getWidth(), this.getHeight(), this);  
+		    }
+		};
+		pnlClass.setLayout(null);
+		bottom.add(pnlClass);
+		pnlClass.setBounds(50, 10, 120, 35);
+		
+		JPanel pnlCourse = new JPanel(){
+			private static final long serialVersionUID = 5365972834168199801L;
+			@Override
+			protected void paintComponent(Graphics g) {
+				Image iconClass = new ImageIcon("images/main/bg_input_kc.png").getImage();
+		        g.drawImage(iconClass, 0, 0, this.getWidth(), this.getHeight(), this);  
+		    }
+		};
+		pnlCourse.setLayout(null);
+		bottom.add(pnlCourse);
+		pnlCourse.setBounds(190, 10, 120, 35);
+		
+		btnBegin = new JButton();//创建按钮对象
+		btnBegin.setBorderPainted(false);//设置边框不可见
+		btnBegin.setContentAreaFilled(false);//设置透明
+		ImageIcon btnImage = new ImageIcon("images/main/btn_begin.png");
+		btnBegin.setIcon(btnImage);//设置图片
+		bottom.add(btnBegin);//添加按钮
+		btnBegin.setBounds(340, -4, btnImage.getIconWidth(), btnImage.getIconHeight());
+		
+		JLabel lblExpected = new JLabel("应到 %d 人",JLabel.CENTER);
+		lblExpected.setBounds(520, 15, 140, 35);
+		bottom.add(lblExpected);
+		JLabel lblActual  = new JLabel("实到 %d 人",JLabel.CENTER);
+		lblActual.setBounds(670, 15, 140, 35);
+		bottom.add(lblActual);
+		
+		
+		contentPane.add(bottom);
+		
+		initData();
+		setBgimg();//设置背景
+	}
+	
+	private void initData() {
+	
+	}
+	
+	//拖动窗体的方法
+	private void setDragable(){
+		frame.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				isDragged = false;
+				frame.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				tmp = new Point(e.getX(), e.getY());//获取窗体位置
+				isDragged = true;
+				frame.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+			}
+		});
+		frame.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				if (isDragged) {
+					loc = new Point(frame.getLocation().x + e.getX()
+							- tmp.x, frame.getLocation().y + e.getY()
+							- tmp.y);
+					frame.setLocation(loc);
+				}
+			}
+		});
+	} 
+	//设置背景
+	public void setBgimg(){
+		lblBackground = new JLabel();
+		lblBackground.setIcon(new ImageIcon("images/main/bg.png"));
+		lblBackground.setBounds(0,0, 1004, 748);
+		frame.add(lblBackground);
+	}
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		
 	}
-
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
+		//按钮按下效果
+//		if(e.getSource()==btnLogin){
+//			btnLogin.setIcon(new ImageIcon("images/login/btn_login_pressed.png"));
+//		}
+		if (e.getSource() == btnMin) {
+			btnMin.setIcon(new ImageIcon("images/login/6.png"));
+		}
+		if (e.getSource() == btnClose) {
+			btnClose.setIcon(new ImageIcon("images/login/7.png"));
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
+		//按钮释放效果
+//		if(e.getSource()==btnLogin){
+//			ImageIcon btnImage = new ImageIcon("images/login/btn_login_normal.png");
+//			btnLogin.setIcon(btnImage);
+//		}
+		if(e.getSource()==btnMin){
+			btnMin.setIcon(new ImageIcon("images/login/5.png"));
+			frame.setExtendedState(JFrame.ICONIFIED);
+		}
+		if(e.getSource()==btnClose){
+			btnClose.setIcon(new ImageIcon("images/login/8.png"));
+			System.exit(0);
+		}
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		
+		//鼠标进入效果
+//		if(e.getSource()==btnLogin){
+//			btnLogin.setIcon(new ImageIcon("images/login/btn_login_pressed.png"));
+//		}
+		if(e.getSource()==btnMin){
+			btnMin.setIcon(new ImageIcon("images/login/5.png"));
+		}
+		if(e.getSource()==btnClose){
+			btnClose.setIcon(new ImageIcon("images/login/8.png"));
+		}
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		
+		//鼠标退出效果
+//		if(e.getSource()==btnLogin){
+//			ImageIcon btnImage = new ImageIcon("images/login/btn_login_normal.png");
+//			btnLogin.setIcon(btnImage);
+//		}
+		if(e.getSource()==btnMin){
+			btnMin.setIcon(new ImageIcon("images/login/4.png"));
+		}
+		if(e.getSource()==btnClose){
+			btnClose.setIcon(new ImageIcon("images/login/7.png"));
+		}
 	}
+	
 }
