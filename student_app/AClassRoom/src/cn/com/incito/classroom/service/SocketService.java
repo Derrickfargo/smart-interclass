8fbf8b53e8e5f50ebf1d0c02b69e69aaa432e0ee
liubo <popoyliu@hotmail.com>
2014-08-06 21:05:30 +0800

    处理启动报错等3个bug

--- student_app/AClassRoom/src/cn/com/incito/classroom/service/SocketService.java
+++ student_app/AClassRoom/src/cn/com/incito/classroom/service/SocketService.java
@@ -1,23 +1,56 @@
 package cn.com.incito.classroom.service;
 
+import cn.com.incito.classroom.R;
+import cn.com.incito.common.utils.ToastHelper;
 import cn.com.incito.socket.core.CoreSocket;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class SocketService extends Service {
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		return null;
-	}
+    public static final String NETWORK_RECEIVER = "cn.com.incito.network.RECEIVER";
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
 
-	@Override
-	public void onCreate() {
+    @Override
+    public void onCreate() {
 //		super.onCreate();
-		CoreSocket.getInstance().start();
+//        CoreSocket.getInstance().start();
+        Thread.setDefaultUncaughtExceptionHandler(
+                new MyUncaughtExceptionHandler());
+        ExecutorService exec = Executors
+                .newCachedThreadPool(new HandlerThreadFactory());
+        exec.execute(CoreSocket.getInstance());
 //        SocketMinaClient.getInstance().start();
-	}
+    }
 
-	
+    class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            System.out.println("caught " + e);
+            Intent intent = new Intent(NETWORK_RECEIVER);
+            intent.putExtra("exception", e.getMessage());
+            sendBroadcast(intent);
+
+        }
+    }
+
+    class HandlerThreadFactory implements ThreadFactory {
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+            System.out.println("eh=" + t.getUncaughtExceptionHandler());
+            return t;
+        }
+    }
+
 }
