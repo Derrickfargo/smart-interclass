6bc959ee653d539319d37ef4e129a5013af7852b
liguangming <510941969@qq.com>
2014-08-06 21:14:59 +0800

    电子绘画版代码，布局文件以及所需图片

---
+++ student_app/AClassRoom/src/cn/com/incito/classroom/canvas/PaletteViewWidget.java
@@ -0,0 +1,261 @@
+package cn.com.incito.classroom.canvas;
+
+
+import java.util.ArrayList;
+
+import cn.com.incito.classroom.R;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PaletteViewWidget extends SurfaceView implements Runnable,
+		SurfaceHolder.Callback {
+	private static final int EARISE_SIZE=30;//橡皮擦大小
+	public Paint mPaint = null;
+	// 画板的坐标以及宽高
+	public int bgBitmapX = 0;
+	public int bgBitmapY = 0;
+	public int bgBitmapHeight = 800;
+	public int bgBitmapWidth = 1280;
+	// 当前的已经选择的画笔参数
+	public int currentPaintTool = 0; // 0和1 0为画线，1为橡皮擦
+
+	public void setCurrentPaintTool(int currentPaintTool) {
+		this.currentPaintTool = currentPaintTool;
+	}
+
+	public int currentColor = Color.BLACK;
+
+	public void setCurrentColor(int currentColor) {
+		this.currentColor = currentColor;
+	}
+
+	public int currentSize = 3; // 1,3,5 画笔粗细
+
+	public void setCurrentSize(int currentSize) {
+		this.currentSize = currentSize;
+	}
+
+	public int currentPaintIndex = -1;
+	public boolean isBackPressed = false;
+	public boolean isForwardPressed = false;
+	// 已选选择单元背景色
+	public int selectedCellColor = 0xffADD8E6;
+	// 存储所有的动作
+	public ArrayList<Action> actionList = null;
+	// 当前的画笔实例
+	public Action curAction = null;
+	// 线程结束标志位
+	public static boolean mLoop = true;
+	SurfaceHolder mSurfaceHolder = null;
+	// 绘图区背景图片
+	Bitmap bgBitmap = null;
+
+	// 临时画板用来显示之前已经绘制过的图像
+	Bitmap newbit = null;
+
+	public PaletteViewWidget(Context context, AttributeSet arr) {
+		super(context, arr);
+
+		mPaint = new Paint();
+		actionList = new ArrayList<Action>();
+		mSurfaceHolder = this.getHolder();
+		mSurfaceHolder.addCallback(this);
+		this.setFocusable(true);
+		mLoop = true;
+		bgBitmap = ((BitmapDrawable) (getResources().getDrawable(R.drawable.bg)))
+				.getBitmap();
+		newbit = Bitmap.createBitmap(bgBitmapWidth, bgBitmapHeight,
+				Config.ARGB_4444);
+		new Thread(this).start();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int antion = event.getAction();
+		if (antion == MotionEvent.ACTION_CANCEL) {
+			return false;
+		}
+
+		float touchX = event.getX();
+		float touchY = event.getY();
+
+		// 点击时
+		if (antion == MotionEvent.ACTION_DOWN) {
+			// 检测点击点是否在主绘图区
+			if (testTouchMainPallent(touchX, touchY)) {
+				setCurAction(getRealX(touchX), getRealY(touchY));
+				clearSpareAction();
+			}
+		}
+		// 拖动时
+		if (antion == MotionEvent.ACTION_MOVE) {
+			if (curAction != null) {
+				curAction.move(getRealX(touchX), getRealY(touchY));
+			}
+		}
+		// 抬起时
+		if (antion == MotionEvent.ACTION_UP) {
+			if (curAction != null) {
+				curAction.move(getRealX(touchX), getRealY(touchY));
+				actionList.add(curAction);
+				currentPaintIndex++;
+				curAction = null;
+			}
+			isBackPressed = false;
+			isForwardPressed = false;
+		}
+		return super.onTouchEvent(event);
+	}
+
+	// 绘图
+	protected void Draw() {
+		Canvas canvas = mSurfaceHolder.lockCanvas();
+		if (mSurfaceHolder == null || canvas == null) {
+			return;
+		}
+		// 填充背景
+		// canvas.drawColor(Color.GREEN);
+		// 画主画板
+		drawMainPallent(canvas);
+
+		mSurfaceHolder.unlockCanvasAndPost(canvas);
+	}
+
+	@Override
+	public void run() {
+		while (mLoop) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			synchronized (mSurfaceHolder) {
+				Draw();
+			}
+		}
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		new Thread(this).start();
+	}
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		mLoop = false;
+	}
+
+
+	// 检测点击事件，是否在主绘图区
+	public boolean testTouchMainPallent(float x, float y) {
+		if (x > bgBitmapX + 2 && y > bgBitmapY + 2
+				&& x < bgBitmapX + bgBitmapWidth - 2
+				&& y < bgBitmapY + bgBitmapHeight - 2) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// 得到当前画笔的类型，并进行实例
+	public void setCurAction(float x, float y) {
+		switch (currentPaintTool) {
+		case 0:
+			curAction = new MyPath(x, y, currentSize, currentColor);
+			break;
+		case 1:
+			curAction = new MyEraser(x, y, EARISE_SIZE, currentColor);
+			break;
+		}
+	}
+
+	// 画主画板
+	public void drawMainPallent(Canvas canvas) {
+		// 设置画笔没有锯齿，空心
+		mPaint.setAntiAlias(true);
+		mPaint.setStyle(Paint.Style.STROKE);
+		// 画板绘图区背景图片
+		canvas.drawBitmap(bgBitmap, bgBitmapX, bgBitmapY, null);
+
+		newbit = Bitmap.createBitmap(bgBitmapWidth, bgBitmapHeight,
+				Config.ARGB_4444);
+		Canvas canvasTemp = new Canvas(newbit);
+		canvasTemp.drawColor(Color.TRANSPARENT);
+		for (int i = 0; i <= currentPaintIndex; i++) {
+			actionList.get(i).draw(canvasTemp);
+		}
+		// 画当前画笔痕迹
+		if (curAction != null) {
+			curAction.draw(canvasTemp);
+		}
+
+		// 在主画板上绘制临时画布上的图像
+		canvas.drawBitmap(newbit, bgBitmapX, bgBitmapY, null);
+		// 画板绘图区边框
+		mPaint.setColor(Color.BLACK);
+		mPaint.setStyle(Paint.Style.STROKE);
+		mPaint.setStrokeWidth(3);
+		canvas.drawRect(bgBitmapX - 2, bgBitmapY - 2, bgBitmapX + bgBitmapWidth
+				+ 2, bgBitmapY + bgBitmapHeight + 2, mPaint);
+	}
+
+	// 根据接触点x坐标得到画板上对应x坐标
+	public float getRealX(float x) {
+
+		return x - bgBitmapX;
+	}
+
+	// 根据接触点y坐标得到画板上对应y坐标
+	public float getRealY(float y) {
+
+		return y - bgBitmapY;
+	}
+
+	public void setBgBitmap(Bitmap bgBitmap) {
+		Matrix mMatrix = new Matrix();
+		mMatrix.reset();
+		float  btWidth = bgBitmap.getWidth();
+		System.out.println("图片宽度:"+btWidth);
+		float  btHeight= bgBitmap.getHeight();
+		System.out.println("图片高度:"+btHeight);
+		float xScale = bgBitmapWidth / btWidth;
+		float yScale = bgBitmapHeight / btHeight;
+		mMatrix.postScale(xScale, yScale);
+		this.bgBitmap = Bitmap.createBitmap(bgBitmap, 0, 0, bgBitmapWidth,
+				bgBitmapHeight, mMatrix, true);
+	}
+
+	// 后退前进完成后，缓存的动作
+	public void clearSpareAction() {
+		for (int i = actionList.size() - 1; i > currentPaintIndex; i--) {
+			actionList.remove(i);
+		}
+	}
+
+	@Override
+	protected void onAttachedToWindow() {
+		super.onAttachedToWindow();
+	}
+	
+}
+
---
+++ student_app/AClassRoom/src/cn/com/incito/classroom/ui/activity/DrawBoxActivity.java
@@ -0,0 +1,301 @@
+package cn.com.incito.classroom.ui.activity;
+
+import cn.com.incito.classroom.R;
+import cn.com.incito.classroom.base.BaseActivity;
+import cn.com.incito.classroom.canvas.PaletteViewWidget;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class DrawBoxActivity extends BaseActivity implements OnClickListener {
+	private PaletteViewWidget iv;
+	private Button mbackcolorBtn;
+	private ImageView mWhiteIco;
+	private ImageView mCanaryYellowIco;
+	private ImageView mEarise;
+	private ImageView mLemonYellow;
+	private ImageView mSalmonOrange;
+	private ImageView mSpanishOrange;
+	private ImageView mMineralOrange;
+	private ImageView mMagenta;
+	private ImageView mHotInk;
+	private ImageView mCarmineRed;
+	private ImageView mPowderBlue;
+	private ImageView mTurquoise;
+	private ImageView mPeacpckBlue;
+	private ImageView mDarkPurple;
+	private ImageView mDarkGreen;
+	private ImageView mMossGreen;
+	private ImageView mParrotGreen;
+	private ImageView mChocolate;
+	private ImageView mBlack;
+	private ImageView mGrey;
+	private RelativeLayout mRelativeLayout;
+	private boolean isBlack = true;
+	private LinearLayout mLinearLayout;
+	private RelativeLayout mBigRelativeLayout;
+	private RelativeLayout mMidRelativeLayout;
+	private RelativeLayout mSmallRelativeLayout;
+	private ImageView mBigCheckIco;
+	private ImageView mMiddleCheckIco;
+	private ImageView mSmallCheckIco;
+	private ImageView mImageView = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.draw_box);
+		mLinearLayout = (LinearLayout) findViewById(R.id.line);
+		mRelativeLayout = (RelativeLayout) findViewById(R.id.main);
+		this.iv = (PaletteViewWidget) this.findViewById(R.id.iv);
+
+		// 背景色选择按钮
+		mbackcolorBtn = (Button) findViewById(R.id.bg_select_btn);
+		mbackcolorBtn.setOnClickListener(this);
+
+		// 颜色选择按钮
+		mWhiteIco = (ImageView) findViewById(R.id.color_white);
+		mWhiteIco.setOnClickListener(this);
+		mCanaryYellowIco = (ImageView) findViewById(R.id.color_canary_yellow);
+		mCanaryYellowIco.setOnClickListener(this);
+		mLemonYellow = (ImageView) findViewById(R.id.ico_lemon_yellow);
+		mLemonYellow.setOnClickListener(this);
+		mSalmonOrange = (ImageView) findViewById(R.id.ico_salmon_orange);
+		mSalmonOrange.setOnClickListener(this);
+		mSpanishOrange = (ImageView) findViewById(R.id.ico_spanish_orange);
+		mSpanishOrange.setOnClickListener(this);
+		mMineralOrange = (ImageView) findViewById(R.id.ico_mineral_orange);
+		mMineralOrange.setOnClickListener(this);
+		mCarmineRed = (ImageView) findViewById(R.id.ico_carmine_red);
+		mCarmineRed.setOnClickListener(this);
+		mHotInk = (ImageView) findViewById(R.id.ico_hot_ink);
+		mHotInk.setOnClickListener(this);
+		mMagenta = (ImageView) findViewById(R.id.ico_magenta);
+		mMagenta.setOnClickListener(this);
+		mPowderBlue = (ImageView) findViewById(R.id.ico_powder_blue);
+		mPowderBlue.setOnClickListener(this);
+		mTurquoise = (ImageView) findViewById(R.id.ico_turquoise);
+		mTurquoise.setOnClickListener(this);
+		mPeacpckBlue = (ImageView) findViewById(R.id.ico_peacpck_blue);
+		mPeacpckBlue.setOnClickListener(this);
+		mDarkPurple = (ImageView) findViewById(R.id.ico_dark_purple);
+		mDarkPurple.setOnClickListener(this);
+		mDarkGreen = (ImageView) findViewById(R.id.ico_dark_green);
+		mDarkGreen.setOnClickListener(this);
+		mMossGreen = (ImageView) findViewById(R.id.ico_moss_green);
+		mMossGreen.setOnClickListener(this);
+		mParrotGreen = (ImageView) findViewById(R.id.ico_parrot_green);
+		mParrotGreen.setOnClickListener(this);
+		mChocolate = (ImageView) findViewById(R.id.ico_chocolate);
+		mChocolate.setOnClickListener(this);
+		mGrey = (ImageView) findViewById(R.id.ico_grey);
+		mGrey.setOnClickListener(this);
+		mBlack = (ImageView) findViewById(R.id.ico_black);
+		mBlack.setOnClickListener(this);
+
+		// 橡皮擦
+		mEarise = (ImageView) findViewById(R.id.earise);
+		mEarise.setOnClickListener(this);
+
+		// 画笔大小选择
+		mBigRelativeLayout = (RelativeLayout) findViewById(R.id.big);
+		mMidRelativeLayout = (RelativeLayout) findViewById(R.id.middle);
+		mSmallRelativeLayout = (RelativeLayout) findViewById(R.id.small);
+		mBigRelativeLayout.setOnClickListener(this);
+		mMidRelativeLayout.setOnClickListener(this);
+		mSmallRelativeLayout.setOnClickListener(this);
+		mBigCheckIco = (ImageView) findViewById(R.id.ico_big_checked);
+		mMiddleCheckIco = (ImageView) findViewById(R.id.ico_middle_checked);
+		mSmallCheckIco = (ImageView) findViewById(R.id.ico_small_checked);
+
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return iv.onTouchEvent(event);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		Translate(mImageView,0);
+		switch (v.getId()) {
+		case R.id.color_white:
+			Translate(mWhiteIco,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(android.R.color.white));
+			break;
+		case R.id.color_canary_yellow:
+			Translate(mCanaryYellowIco,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.canary_yellow));
+			break;
+		case R.id.ico_lemon_yellow:
+			Translate(mLemonYellow,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.lemon_yellow));
+			break;
+		case R.id.ico_salmon_orange:
+			Translate(mSalmonOrange,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.salmon_orange));
+			break;
+		case R.id.ico_spanish_orange:
+			Translate(mSpanishOrange,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.spanish_orange));
+			break;
+		case R.id.ico_mineral_orange:
+			Translate(mMineralOrange,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.mineral_orange));
+			break;
+		case R.id.ico_carmine_red:
+			Translate(mCarmineRed,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.carmine_red));
+			break;
+		case R.id.ico_hot_ink:
+			Translate(mHotInk,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.hot_ink));
+			break;
+		case R.id.ico_magenta:
+			Translate(mMagenta,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.magenta));
+			break;
+		case R.id.ico_powder_blue:
+			Translate(mPowderBlue,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.powder_blue));
+			break;
+		case R.id.ico_turquoise:
+			Translate(mTurquoise,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.turquoise));
+			break;
+		case R.id.ico_peacpck_blue:
+			Translate(mPeacpckBlue,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.peacpck_blue));
+			break;
+		case R.id.ico_dark_purple:
+			Translate(mDarkPurple,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.dark_purple));
+			break;
+		case R.id.ico_dark_green:
+			Translate(mDarkGreen,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.dark_green));
+			break;
+		case R.id.ico_moss_green:
+			Translate(mMossGreen,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.moss_green));
+			break;
+		case R.id.ico_parrot_green:
+			Translate(mParrotGreen,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.parrot_green));
+			break;
+		case R.id.ico_chocolate:
+			Translate(mChocolate,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.chocolate));
+			break;
+		case R.id.ico_black:
+			Translate(mBlack,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.black));
+			break;
+		case R.id.ico_grey:
+			Translate(mGrey,-0.25f);
+			iv.setCurrentPaintTool(0);
+			iv.setCurrentColor(getResources().getColor(R.color.grey));
+			break;
+		case R.id.earise:// 橡皮擦
+			iv.setCurrentPaintTool(1);
+			break;
+		case R.id.bg_select_btn:// 背景选择
+			if (isBlack) {
+				mRelativeLayout.setBackgroundResource(R.drawable.bg_white);
+				iv.setBgBitmap(((BitmapDrawable) (getResources()
+						.getDrawable(R.drawable.bg_white))).getBitmap());
+				mbackcolorBtn.setBackgroundResource(R.drawable.bg_cgbg_bb);
+				isBlack = false;
+			} else {
+				mRelativeLayout.setBackgroundResource(R.drawable.bg);
+				iv.setBgBitmap(((BitmapDrawable) (getResources()
+						.getDrawable(R.drawable.bg))).getBitmap());
+				mbackcolorBtn.setBackgroundResource(R.drawable.bg_cgbg_white);
+				isBlack = true;
+			}
+			break;
+
+		case R.id.big:// 粗笔
+			iv.setCurrentSize(5);
+			mBigCheckIco.setVisibility(View.VISIBLE);
+			mMiddleCheckIco.setVisibility(View.GONE);
+			mSmallCheckIco.setVisibility(View.GONE);
+			break;
+		case R.id.middle:// 中等笔
+			iv.setCurrentSize(3);
+			mBigCheckIco.setVisibility(View.GONE);
+			mMiddleCheckIco.setVisibility(View.VISIBLE);
+			mSmallCheckIco.setVisibility(View.GONE);
+			break;
+		case R.id.small:// 细笔
+			iv.setCurrentSize(1);
+			mBigCheckIco.setVisibility(View.GONE);
+			mMiddleCheckIco.setVisibility(View.GONE);
+			mSmallCheckIco.setVisibility(View.VISIBLE);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * @param imageView
+	 *            图片动画效果
+	 */
+	public void Translate(ImageView imageView, float index) {
+		if (imageView == null) {
+			return;
+		}
+		AnimationSet animationSet = new AnimationSet(true);
+		TranslateAnimation translateAnimation = new TranslateAnimation(
+				Animation.RELATIVE_TO_SELF, 0f, // X轴的开始位置
+				Animation.RELATIVE_TO_SELF, 0f, // X轴的结束位置
+				Animation.RELATIVE_TO_SELF, 0f, // Y轴的开始位置
+				Animation.RELATIVE_TO_SELF, index); // Y轴的结束位置
+		translateAnimation.setDuration(200);
+		animationSet.addAnimation(translateAnimation);
+		animationSet.setFillAfter(true);
+		animationSet.setFillBefore(false);
+		if (index < 0) {
+			mImageView = imageView;
+		}
+		imageView.startAnimation(animationSet);
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+}
