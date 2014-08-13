b753ca7a7981506d9070861e4a91b94f000432d8
liguangming <510941969@qq.com>
2014-08-13 17:30:53 +0800

    增加了保存作业后返回的信息

---
+++ student_app/AClassRoom/src/cn/com/incito/socket/handler/SavePaperResultHandler.java
@@ -0,0 +1,12 @@
+package cn.com.incito.socket.handler;
+
+import cn.com.incito.socket.core.MessageHandler;
+
+public class SavePaperResultHandler extends MessageHandler {
+
+	@Override
+	protected void handleMessage() {
+		
+	}
+
+}
