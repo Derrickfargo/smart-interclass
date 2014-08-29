8d8d0398c2b94b25a1b488893f60b9768f6a2f49
liushiping <6423669@qq.com>
2014-08-29 15:39:09 +0800

    修改界面，没绑定课桌时，显示绑定课桌的图片

--- teacher_app/src/cn/com/incito/interclass/ui/PreparePanel.java
+++ teacher_app/src/cn/com/incito/interclass/ui/PreparePanel.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import cn.com.incito.interclass.po.Device;
 import cn.com.incito.interclass.po.Group;
-import cn.com.incito.interclass.po.Student;
 import cn.com.incito.interclass.po.Table;
 import cn.com.incito.server.api.Application;
 
@@ -18,6 +19,8 @@ public class PreparePanel extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = 6316121486627261595L;
+	private static final String ICON_NO_DESK = "images/main/bg_binding_desk.png";
+	private JLabel lblNoDesk;
 	private Application app = Application.getInstance();
 	/**
 	 * 当前教室所有Table，初始化界面时初始化本属性
@@ -56,10 +59,21 @@ public class PreparePanel extends JPanel{
 			i++;
 			y += 220;
 		}
+		lblNoDesk = new JLabel();
+		ImageIcon icon = new ImageIcon(ICON_NO_DESK);
+		lblNoDesk.setIcon(icon);
+		lblNoDesk.setBounds(288, 235, 300, 160);
+		lblNoDesk.setVisible(false);
+		add(lblNoDesk);
 	}
 
 	public void refresh() {
 		initData();
+		if (groupList.size() == 0) {//未绑定 
+			lblNoDesk.setVisible(true);
+			return;
+		}
+		lblNoDesk.setVisible(false);
 		// 遍历内存模型，绑定到物理模型
 		for (int i = 0; i < groupList.size(); i++) {// 遍历分组内存模型
 			Group group = groupList.get(i);
