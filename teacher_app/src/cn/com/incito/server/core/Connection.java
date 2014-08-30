334e6acce63877f526def2a02927746b83f063e6
liushiping <6423669@qq.com>
2014-08-30 15:31:13 +0800

    客服端断网后，服务端检测到并且刷新UI。通知其他设备

--- teacher_app/src/cn/com/incito/server/core/Connection.java
+++ teacher_app/src/cn/com/incito/server/core/Connection.java
@@ -1,11 +1,23 @@
 package cn.com.incito.server.core;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.alibaba.fastjson.JSONObject;
+
+import cn.com.incito.interclass.po.Device;
+import cn.com.incito.interclass.po.Group;
+import cn.com.incito.interclass.po.Student;
+import cn.com.incito.interclass.po.Table;
 import cn.com.incito.server.api.Application;
+import cn.com.incito.server.message.DataType;
+import cn.com.incito.server.message.MessagePacking;
+import cn.com.incito.server.utils.BufferUtils;
+import cn.com.incito.server.utils.JSONUtils;
 
 /**
  * 单个客户端连接，主要管理心跳
@@ -56,6 +68,61 @@ public class Connection {
 		if (channel != null && channel.isConnected()) {
 			try {
 				channel.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//刷新界面
+		doLogout();
+	}
+	
+	private void doLogout(){
+		Application app = Application.getInstance();
+		Device device = app.getImeiDevice().get(imei);
+		if (device == null) {
+			return;
+		}
+		Table table = app.getDeviceTable().get(device.getId());
+		if (table == null) {
+			return;
+		}
+		Group group = app.getTableGroup().get(table.getId());
+		List<Student> students = app.getStudentByImei(imei);
+		if (students != null) {
+			for (Student student : students) {
+				for (Student aStudent : group.getStudents()) {
+					if (student.getName().equals(aStudent.getName())
+							&& student.getNumber().equals(aStudent.getNumber())) {
+						aStudent.setLogin(false);
+					}
+				}
+			}
+		}
+		app.removeLoginStudent(imei);
+		app.getOnlineDevice().remove(imei);
+		Application.getInstance().getClientChannel().remove(imei);
+		app.refresh();// 更新UI
+		
+		//向其他设备发送退出通知
+		List<SocketChannel> channels = Application.getInstance().getClientChannelByGroup(group.getId());
+		JSONObject json = new JSONObject();
+		json.put("code", JSONUtils.SUCCESS);
+		json.put("data", group);
+		sendResponse(json.toJSONString(), channels);
+	}
+	
+	private void sendResponse(String json,List<SocketChannel> channels) {
+		for (SocketChannel channel : channels) {
+			MessagePacking messagePacking = new MessagePacking(Message.MESSAGE_STUDENT_LOGIN);
+	        messagePacking.putBodyData(DataType.INT, BufferUtils.writeUTFString(json));
+	        byte[] messageData = messagePacking.pack().array();
+	        ByteBuffer buffer = ByteBuffer.allocate(messageData.length);
+	        buffer.put(messageData);
+	        buffer.flip();
+			try {
+				if (channel.isConnected()) { 
+					channel.write(buffer);
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
