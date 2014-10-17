428e88051cd479718816be33023cbb499d53bbac
liguangming <510941969@qq.com>
2014-10-17 16:52:04 +0800

    安装更新程序时 程序会退出

--- dreamclass/student_app/AClassRoom/src/cn/com/incito/classroom/utils/UpdateManager.java
+++ dreamclass/student_app/AClassRoom/src/cn/com/incito/classroom/utils/UpdateManager.java
@@ -1,6 +1,5 @@
 package cn.com.incito.classroom.utils;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,12 +16,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.ProgressBar;
 import cn.com.incito.classroom.R;
+import cn.com.incito.classroom.base.AppManager;
 
 public class UpdateManager {
 
@@ -63,9 +61,7 @@ public class UpdateManager {
 				mProgress.setProgress(progress);
 				break;
 			case DOWN_OVER:
-//				installApk();
-				File apkfile = new File(saveFileName);
-				install(apkfile.getAbsolutePath());
+				installApk();
 				break;
 			default:
 				break;
@@ -169,111 +165,7 @@ public class UpdateManager {
 		Intent i = new Intent(Intent.ACTION_VIEW);
 		i.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
 		mContext.startActivity(i);
-	}
-
-	/**
-	 * 打开APK文件
-	 * 
-	 * @param file
-	 */
-	private void openFile(File file) {
-		// TODO Auto-generated method stub
-		Log.i("OpenFile", file.getName());
-		Intent intent = new Intent();
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		intent.setAction(android.content.Intent.ACTION_VIEW);
-		intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
-		mContext.startActivity(intent);
-	}
-
-	// 方法体可以在任何地方可套用，不用修改任何东东哦！
-	/**
-	 * 强制安装APK
-	 * @param apkAbsolutePath
-	 * @return
-	 */
-	public String install(String apkAbsolutePath) {
-
-		String[] args = { "pm", "install", "-r", apkAbsolutePath };
-
-		String result = "";
-
-		ProcessBuilder processBuilder = new ProcessBuilder(args);
-
-		Process process = null;
-
-		InputStream errIs = null;
-
-		InputStream inIs = null;
-		try {
-
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-			int read = -1;
-
-			process = processBuilder.start();
-
-			errIs = process.getErrorStream();
-
-			while ((read = errIs.read()) != -1) {
-
-				baos.write(read);
-
-			}
-
-			baos.write("/n".getBytes());
-
-			inIs = process.getInputStream();
-
-			while ((read = inIs.read()) != -1) {
-
-				baos.write(read);
-
-			}
-
-			byte[] data = baos.toByteArray();
-
-			result = new String(data);
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
-		} finally {
-
-			try {
-
-				if (errIs != null) {
-
-					errIs.close();
-
-				}
-
-				if (inIs != null) {
-
-					inIs.close();
-
-				}
-
-			} catch (IOException e) {
-
-				e.printStackTrace();
-
-			}
-
-			if (process != null) {
-				process.destroy();
-
-			}
-
-		}
-
-		return result;
-
+		AppManager.getAppManager().AppExit(mContext);
 	}
 
 }
