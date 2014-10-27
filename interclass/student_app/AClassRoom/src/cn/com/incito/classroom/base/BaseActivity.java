7e8ee5b24b12baab2a51cd6bf4134044a88e9bc1
'hm' <1977979333@qq.com>
2014-10-27 14:32:21 +0800

    选择班级界面 选择分组界面 创建分组界面

--- interclass/student_app/AClassRoom/src/cn/com/incito/classroom/base/BaseActivity.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/classroom/base/BaseActivity.java
@@ -3,7 +3,6 @@ package cn.com.incito.classroom.base;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/GroupListHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/GroupListHandler.java
@@ -4,7 +4,7 @@ package cn.com.incito.socket.handler;
 import java.util.List;
 
 import cn.com.incito.classroom.base.MyApplication;
-import cn.com.incito.classroom.vo.GroupVo;
+//import cn.com.incito.classroom.vo.GroupVo;
 import cn.com.incito.common.utils.UIHelper;
 import cn.com.incito.socket.core.MessageHandler;
 import cn.com.incito.wisdom.sdk.log.WLog;
@@ -16,10 +16,10 @@ public class GroupListHandler extends MessageHandler {
 
 	@Override
 	protected void handleMessage() {
-		List<GroupVo> groupList =(List<GroupVo>) data.get("group");
-		WLog.i(GroupListHandler.class, "收到显示小组列表命令");
-		UIHelper.getInstance().showGroupListActivity(groupList);
-		MyApplication.getInstance().lockScreen(false);
+//		List<GroupVo> groupList =(List<GroupVo>) data.get("group");
+//		WLog.i(GroupListHandler.class, "收到显示小组列表命令");
+//		UIHelper.getInstance().showGroupListActivity(groupList);
+//		MyApplication.getInstance().lockScreen(false);
 	}
 
 }
