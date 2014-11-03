51884fc0e7ffc5625ffe4f58d2bc2201e424c717
'hm' <1977979333@qq.com>
2014-11-03 17:52:16 +0800

    选择分组与后台的交互

--- interclass/student_app/AClassRoom/src/cn/com/incito/classroom/ui/activity/SelectGroupActivity.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/classroom/ui/activity/SelectGroupActivity.java
@@ -1,8 +1,8 @@
 package cn.com.incito.classroom.ui.activity;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,7 +13,16 @@ import android.widget.ListView;
 import cn.com.incito.classroom.R;
 import cn.com.incito.classroom.adapter.SelectGroupAdapter;
 import cn.com.incito.classroom.base.BaseActivity;
+import cn.com.incito.classroom.base.MyApplication;
+import cn.com.incito.classroom.vo.Device;
 import cn.com.incito.classroom.vo.Group;
+import cn.com.incito.socket.core.CoreSocket;
+import cn.com.incito.socket.core.Message;
+import cn.com.incito.socket.message.DataType;
+import cn.com.incito.socket.message.MessagePacking;
+import cn.com.incito.socket.utils.BufferUtils;
+
+import com.alibaba.fastjson.JSONObject;
 
 public class SelectGroupActivity extends BaseActivity implements
 		OnClickListener {
@@ -27,10 +36,11 @@ public class SelectGroupActivity extends BaseActivity implements
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_select_group);
-
+		
+		Intent intent = new Intent();
+		setGroupData(intent.getStringExtra("data"));
 		// 初始化UI组件
 		initViews();
-
 	}
 
 	@Override
@@ -53,6 +63,7 @@ public class SelectGroupActivity extends BaseActivity implements
 		btn_create_group.setOnClickListener(this);
 
 		select_group_listview = (ListView) findViewById(R.id.select_group_listview);
+		
 
 		// 设置每项点击事件 发送请求  并且更新界面
 		select_group_listview.setOnItemClickListener(new OnItemClickListener() {
@@ -60,13 +71,35 @@ public class SelectGroupActivity extends BaseActivity implements
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
+				//1.封装要发送的数据 小组ID  学生ID
+				int groupId = groupList.get(position).getId();
+				String deviceId = MyApplication.deviceId;
+				
+				//2.判断当前设备是否已经加入该分组
+				for(int i = 0; i < groupList.size(); i++){
+					Group group = groupList.get(i);
+					List<Device> deviceList = group.getDevices();
+					for(int j = 0; i < deviceList.size(); j++){
+						Device device = deviceList.get(j);
+						if(!device.getImei().equals(deviceId)){//没有加入分组才发送请求 
+							JSONObject jsonObject = new JSONObject();
+							jsonObject.put("groupId", groupId);
+							jsonObject.put("imei", deviceId);
+							
+							MessagePacking messagePacking = new MessagePacking(Message.MESSAGE_GROUP_JOIN);
+							messagePacking.putBodyData(DataType.INT,BufferUtils.writeUTFString(jsonObject.toJSONString()));
+							CoreSocket.getInstance().sendMessage(messagePacking);
+							
+							MyApplication.LOGGER.debug(System.currentTimeMillis() + "开始发送分组请求 ");
+						}
+					}
+				}
 				
 			}
 		});
 		
-		setGroupData();
-
 		selectGroupAdapter = new SelectGroupAdapter(this);
+		selectGroupAdapter.notifyDataSetChanged();
 
 		select_group_listview.setAdapter(selectGroupAdapter);
 
@@ -76,28 +109,12 @@ public class SelectGroupActivity extends BaseActivity implements
 		return groupList;
 	}
 
-	private void setGroupData() {
-		// 初始化假数据
-		List<String> memberNames = new ArrayList<String>();
-		memberNames.add("张三 ");
-		memberNames.add("李四");
-		memberNames.add("王五 ");
-
-		Group g = new Group();
-		g.setName("潜水小组");
-		groupList = new ArrayList<Group>();
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
-		groupList.add(g);
+	/**
+	 * 设置数据
+	 */
+	@SuppressWarnings("unchecked")
+	private void setGroupData(String json) {
+		JSONObject jsonObject = JSONObject.parseObject(json);
+		groupList = (List<Group>) jsonObject.get("group");
 	}
-
 }
