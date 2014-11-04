b1a360dc3d766c667aee709fcb72dbc53669c7a7
'hm' <1977979333@qq.com>
2014-11-04 13:54:48 +0800

    添加几个handler

--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/GroupListHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/GroupListHandler.java
@@ -1,15 +1,13 @@
 package cn.com.incito.socket.handler;
 
-
 //import cn.com.incito.classroom.vo.GroupVo;
 
-import com.alibaba.fastjson.JSONObject;
-
+import cn.com.incito.classroom.base.AppManager;
 import cn.com.incito.common.utils.UIHelper;
 import cn.com.incito.socket.core.MessageHandler;
+
 /**
- * 获取组成员列表hanlder
- * Created by liushiping on 2014/7/28.
+ * 获取组成员列表hanlder Created by liushiping on 2014/7/28.
  */
 public class GroupListHandler extends MessageHandler {
 
@@ -17,11 +15,13 @@ public class GroupListHandler extends MessageHandler {
 	protected void handleMessage() {
 		
 		if(0 == data.getIntValue("code")){
-			JSONObject json = data.getJSONObject("data");
-			UIHelper.getInstance().showSelectGroup(json.toString());
+//			JSONObject json = data.getJSONObject("data");
+			if(AppManager.getAppManager().currentActivity().getComponentName().getClassName().equals("WaitForOtherMembersActivity")){
+				UIHelper.getInstance().showWaitOtherMembers(data.toString());
+			}else{
+				UIHelper.getInstance().showGroupSelect(data.toString());
+			}
 		}
 		
-//		UIHelper.getInstance().showGroupListActivity();
 	}
-
 }
