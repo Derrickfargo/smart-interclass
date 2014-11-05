f5ed51f2ca4b663f716c02bb5c94f090c6cb8106
'hm' <'1977979333@qq.com'>
2014-11-05 13:05:34 +0800

    更改数据传输的格式转换

--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/DeleteGroupHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/DeleteGroupHandler.java
@@ -3,12 +3,15 @@ package cn.com.incito.socket.handler;
 import cn.com.incito.common.utils.UIHelper;
 import cn.com.incito.socket.core.MessageHandler;
 
+import com.alibaba.fastjson.JSONArray;
+
 public class DeleteGroupHandler extends MessageHandler {
 
 	@Override
 	protected void handleMessage() {
 		if(0 == data.getIntValue("code")){
-				UIHelper.getInstance().showGroupSelect(data.toString());
+			JSONArray jsonArray = data.getJSONArray("data");
+				UIHelper.getInstance().showGroupSelect(jsonArray.toJSONString());
 		}
 
 	}
