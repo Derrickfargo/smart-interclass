e2b04c9a2599689943ccbd19105f5b172222fae9
liguangming <510941969@qq.com>
2014-11-07 15:01:33 +0800

    增加了服务IP设置

--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/LoginHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/LoginHandler.java
@@ -1,6 +1,9 @@
 package cn.com.incito.socket.handler;
 
+import android.widget.Toast;
+import cn.com.incito.classroom.base.AppManager;
 import cn.com.incito.classroom.base.MyApplication;
+import cn.com.incito.classroom.constants.Constants;
 import cn.com.incito.classroom.vo.Student;
 import cn.com.incito.common.utils.UIHelper;
 import cn.com.incito.socket.core.ConnectionManager;
@@ -18,10 +21,14 @@ public class LoginHandler extends MessageHandler {
 		WLog.i(LoginHandler.class, "连接建立成功,开始启动心跳!");
 		String server_ip = data.getString("server_ip");
 		String server_port = data.getString("server_port");
+		Constants.setSERVER_IP(server_ip);
+		Constants.setSERVER_PORT(server_port);
 		//TODO 有可能这台pad没有绑定学生，
 		Student student = data.getObject("student", Student.class);
+		if(student==null){
+			Toast.makeText(AppManager.getAppManager().currentActivity(), "该pad还未绑定学生，请先绑定学生", Toast.LENGTH_LONG).show();
+		}
 		MyApplication.getInstance().setStudent(student);
-		//TODO 需要添加server_ip，server_port到Application
 		UIHelper.getInstance().showClassReadyActivity();
 		//启动心跳检测
 		ConnectionManager.getInstance(message.getChannel());
