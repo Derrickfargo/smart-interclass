47adb7ca141aafd82a204100fd829ce1dab4fd62
liguangming <510941969@qq.com>
2014-11-27 18:06:57 +0800

    更改了日志内容

--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/SavePaperResultHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/SavePaperResultHandler.java
@@ -14,7 +14,7 @@ public class SavePaperResultHandler extends MessageHandler {
 	protected void handleMessage() {
 		MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"--SavePaperResultHandler.class:保存作业完成后返回信息"+data.toJSONString());
 		if (data.getIntValue("code") == 0) {
-			WLog.i(SavePaperResultHandler.class, "paper保存成功");
+			MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"提交作业成功");
 			MyApplication.getInstance().setSubmitPaper(true);
 			MyApplication.getInstance().lockScreen(true);
 		}
