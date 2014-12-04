5ff3f48f8893ebb1a0502f7bd77865bc449ab4b4
1977979333@qq.com <619822hm>
2014-12-04 13:54:20 +0800

    抢答器

--- dreamclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/ResponderHandler.java
+++ dreamclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/ResponderHandler.java
@@ -3,26 +3,26 @@ package cn.com.incito.socket.handler;
 import cn.com.incito.classroom.base.MyApplication;
 import cn.com.incito.common.utils.AndroidUtil;
 import cn.com.incito.common.utils.UIHelper;
+import cn.com.incito.socket.core.Message;
 import cn.com.incito.socket.core.MessageHandler;
 
 /**
  * 收到抢答消息的处理类
+ * 
  * @author hm
  *
  */
 public class ResponderHandler extends MessageHandler {
+	
+	@Override
+	public void handleMessage(Message msg) {
+		handleMessage();
+	}
 
 	@Override
 	protected void handleMessage() {
-		MyApplication.Logger.debug(AndroidUtil.getCurrentTime() + "::ResponderHandler::" + "收到抢答消息::" + data.toJSONString());
-		
-		int code = data.getIntValue("code");
-		
-		if(0 == code){
-			//屏幕解锁
-			MyApplication.getInstance().setLockScreen(false);
-			//进入抢答界面
-			UIHelper.getInstance().showResponderActivity();
-		}
+		MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+ "::ResponderHandler::" + "收到抢答消息");
+		// 进入抢答界面
+		UIHelper.getInstance().showResponderActivity();
 	}
 }
