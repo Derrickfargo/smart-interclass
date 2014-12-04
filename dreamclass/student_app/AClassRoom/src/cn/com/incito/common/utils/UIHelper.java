429ece1126b7fed94693d788cab477fbd3cc9660
liguangming <510941969@qq.com>
2014-12-04 11:24:32 +0800

    增加了互评设置

--- dreamclass/student_app/AClassRoom/src/cn/com/incito/common/utils/UIHelper.java
+++ dreamclass/student_app/AClassRoom/src/cn/com/incito/common/utils/UIHelper.java
@@ -8,6 +8,7 @@ import cn.com.incito.classroom.base.MyApplication;
 import cn.com.incito.classroom.constants.Constants;
 import cn.com.incito.classroom.ui.activity.BindDeskActivity;
 import cn.com.incito.classroom.ui.activity.DrawBoxActivity;
+import cn.com.incito.classroom.ui.activity.EvaluateActivity;
 import cn.com.incito.classroom.ui.activity.WaitingActivity;
 
 import com.alibaba.fastjson.JSONObject;
@@ -18,7 +19,8 @@ public class UIHelper {
 	private WaitingActivity waitingActivity;
 	private BindDeskActivity bindDeskActivity;
 	private DrawBoxActivity drawBoxActivity;
-
+	private EvaluateActivity evaluateActivity;
+	
 	private UIHelper() {
 		app = MyApplication.getInstance();
 	}
@@ -154,4 +156,15 @@ public class UIHelper {
 		intent.setAction(Constants.ACTION_SHOW_EDIT_GROUP);
 		app.startActivity(intent);
 	}
+
+	
+	public EvaluateActivity getEvaluateActivity() {
+		return evaluateActivity;
+	}
+
+	
+	public void setEvaluateActivity(EvaluateActivity evaluateActivity) {
+		this.evaluateActivity = evaluateActivity;
+	}
+	
 }
