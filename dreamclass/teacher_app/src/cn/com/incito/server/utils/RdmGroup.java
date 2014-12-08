d997e6c15ef68be3d935b2d859a142897b666047
caihuayu0322 <837522223@qq.com>
2014-12-08 20:24:36 +0800

    随机分组算法

---
+++ dreamclass/teacher_app/src/cn/com/incito/server/utils/RdmGroup.java
@@ -0,0 +1,41 @@
+package cn.com.incito.server.utils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+import cn.com.incito.interclass.po.Student;
+import cn.com.incito.server.api.Application;
+
+public class RdmGroup {
+	public static Queue<List<Student>>  getStudentQue(){
+		Application app = Application.getInstance();
+		Set<String> onlinePads = app.getOnlineDevice();//在线设备集合
+		Set<Student> onlineStu = app.getOnlineStudent();//在线学生集合
+		Queue< List<Student>> studentQue = getStuQue(onlinePads.size());
+		
+		for(Student student: onlineStu){//将学生遍历进设备队列中
+			List<Student> students = studentQue.poll();
+			students.add(student);
+			studentQue.offer(students);
+		}
+		
+		return studentQue;
+	}
+	/**
+	 * 
+	 * @param size
+	 * @return
+	 */
+	private static Queue<List<Student>> getStuQue(int size) {
+		Queue<List<Student>>stuQue = new LinkedList<List<Student>>();//FIFO
+		for (int i = 0; i < size; i++) {
+			List<Student>students = new ArrayList<Student>();
+			stuQue.add(students);
+		}
+		return stuQue;
+	}
+
+}
