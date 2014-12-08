24de0a43e423d1ac80e3b0b9c0af25b89fa17a9f
liushiping <JOHN@Lenovo-PC>
2014-12-08 11:03:23 +0800

    修改作业互评算法

--- dreamclass/teacher_app/src/cn/com/incito/server/utils/PeerFeedbackUtils.java
+++ dreamclass/teacher_app/src/cn/com/incito/server/utils/PeerFeedbackUtils.java
@@ -3,24 +3,28 @@ package cn.com.incito.server.utils;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Queue;
+import java.util.Set;
 
 import cn.com.incito.interclass.po.Quiz;
+import cn.com.incito.server.api.Application;
+import cn.com.incito.server.config.AppConfig;
 
 public class PeerFeedbackUtils {
-	private static int count = 3;// 每份作业出现次数
-	
 	public static Queue<List<Quiz>> getQuizQueue() {
-		List<Quiz> quizList = getQuizList();
-		List<String> padList = getPadList();
-		Queue<List<Quiz>> quizQueue = initQuizQueue();
+		Application app = Application.getInstance();
+		List<Quiz> quizList = app.getQuizList();
+		Set<String> onlinePads = app.getOnlineDevice();
+		Queue<List<Quiz>> quizQueue = initQuizQueue(onlinePads);
 
-		if (padList.size() > quizList.size()) {
-			++count;
+		int quizCount = getQuizRepeatCount();
+		if (onlinePads.size() > quizList.size()) {
+			++quizCount;
 		}
-		
-		for (Quiz quiz : quizList) {//所有作业
-			for (int j = 0; j < count; j++) {//每个作业出现count次
+
+		for (Quiz quiz : quizList) {// 所有作业
+			for (int j = 0; j < quizCount; j++) {
 				List<Quiz> quizzes = quizQueue.poll();
 				quizzes.add(quiz);
 				quizQueue.offer(quizzes);
@@ -29,31 +33,16 @@ public class PeerFeedbackUtils {
 		return quizQueue;
 	}
 
-	private static Queue<List<Quiz>> initQuizQueue() {
+	private static int getQuizRepeatCount() {
+		Properties props = AppConfig.getProperties();
+		return Integer.parseInt(props.getProperty("quiz_repeat_count"));
+	}
+
+	private static Queue<List<Quiz>> initQuizQueue(Set<String> onlinePads) {
 		Queue<List<Quiz>> quizQueue = new LinkedList<List<Quiz>>();
-		List<String> padList = getPadList();
-		for (int i = 0; i < padList.size(); i++) {
+		for (int i = 0; i < onlinePads.size(); i++) {
 			quizQueue.offer(new ArrayList<Quiz>());
 		}
 		return quizQueue;
-	}
-	
-	private static List<Quiz> getQuizList() {
-		List<Quiz> quizList = new ArrayList<Quiz>();
-		for (int i = 0; i < 32; i++) {
-			Quiz quiz = new Quiz();
-			quiz.setName("quiz" + (i + 1));
-			quizList.add(quiz);
-		}
-		return quizList;
-	}
-
-	//Pad的IMEI
-	private static List<String> getPadList() {
-		List<String> padList = new ArrayList<String>();
-		for (int i = 0; i < 32; i++) {
-			padList.add(String.valueOf(i + 1));
-		}
-		return padList;
 	}
 }
