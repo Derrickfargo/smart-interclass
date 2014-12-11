d02a5eb6a5a28d832f288bb8ce6935e77b4f7e4e
liguangming <510941969@qq.com>
2014-12-11 21:41:48 +0800

    解决出现UncaughtException时 程序退出的问题

--- interclass/student_app/AClassRoom/src/cn/com/incito/classroom/exception/AppUncaughtException.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/classroom/exception/AppUncaughtException.java
@@ -16,6 +16,7 @@ import java.lang.Thread.UncaughtExceptionHandler;
 import android.content.Context;
 import android.util.Log;
 import cn.com.incito.classroom.base.AppManager;
+import cn.com.incito.classroom.base.MyApplication;
 import cn.com.incito.classroom.utils.ApiClient;
 import cn.com.incito.common.utils.AndroidUtil;
 
@@ -52,40 +53,10 @@ public class AppUncaughtException implements UncaughtExceptionHandler {
 		Log.e("程序挂掉了","");
 		// 把错误的堆栈信息 获取出来
 		final String errorinfo = getErrorInfo(arg1);
-		// 打印到控制台上异常信息
-//		System.err.println(errorinfo);
-		// 获取异常出现的时间
-//		SimpleDateFormat format1 = new SimpleDateFormat("yyyy年MM月dd日HH:mm:ss");
-//		final String currentTimeString = format1.format(new Date(System.currentTimeMillis()));
-//		// 获取当前应用名称
-//		final String _ApplicationName = AndroidUtil.getApplicationName(context);
-//		// 获取当前应用版本号
-//		String _AppVersion = AndroidUtil.getAppVersion(context);
-		// 获取当前应的屏幕大小
-		// String _ScreenSize = AndroidUtil.getScreenSize((Activity) context);
-		// 获取ChannelId "\nChannelId:" + _ChannelId +
-		// int _ChannelId = AndroidUtil.getChannelId(context);
-		// 获取当前设备ID
-//		String _DeviceId = AndroidUtil.getDeviceId(context);
-		// 获取当前android版本
-//		String _Version = AndroidUtil.getVersion();
-		// 获取当前手机屏幕密度
-		// String _Dpi = AndroidUtil.getDpi((Activity) context);+ "\n屏幕大小:" + _ScreenSize +
-		// "\n屏幕密度:" + _Dpi
-//		String _Model = AndroidUtil.getModel();
-//		String _UserAgent = AndroidUtil.getUserAgent();
-//		final String _AppInfo = "应用名称:" + _ApplicationName + "\n应用版本号:" + _AppVersion + "\n设备ID:" + _DeviceId + "\nAndroid版本:" + _Version + "\nModel:" + _Model + "\nUserAgent:" + _UserAgent + "\n";
-
-//		if (AndroidUtil.isSDCardAvailable()) {
-//			FileUtils.writeFileToSDCardFromString(_AppInfo, _ApplicationName + ".txt");
-//			Log.i("写入SD卡",_ApplicationName+".txt");
-//		}
 		if (AndroidUtil.isNetworkAvailable(context)) {
 			ApiClient.uploadErrorLog(errorinfo);
 		}
-
-		AppManager.getAppManager().AppExit(context);;
-		android.os.Process.killProcess(android.os.Process.myPid());
+		MyApplication.Logger.debug("AppUncaughtException:"+errorinfo);
 	}
 
 	/**
