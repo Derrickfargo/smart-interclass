b699ede9c5acd9ed2e9f06264d5167f186c3b948
liguangming <510941969@qq.com>
2014-12-11 18:04:36 +0800

    增加日志

--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/SavePaperResultHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/SavePaperResultHandler.java
@@ -18,14 +18,18 @@ public class SavePaperResultHandler extends MessageHandler {
 		MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"--SavePaperResultHandler.class:保存作业完成后返回信息"+data.toJSONString());
 		if (data.getIntValue("code") == 0) {
 			MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"提交作业成功");
+			MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"提交作业后关闭进度条");
 			UIHelper.getInstance().getDrawBoxActivity().cancleTask();
 			MyApplication.getInstance().setSubmitPaper(true);
 			MyApplication.getInstance().lockScreen(true);
+			MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"提交作业后删除本地图片");
 			File f = new File("/sdcard/", "temp.jpg");
 			if(f.exists()){
 				f.delete();
 			}
+			MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"提交作业后关闭绘画板");
 			AppManager.getAppManager().currentActivity().finish();
+			MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+"提交作业后显示正在上课界面");
 			if (UIHelper.getInstance().getClassingActivity() == null) {
 				UIHelper.getInstance().showClassingActivity();
 			}
