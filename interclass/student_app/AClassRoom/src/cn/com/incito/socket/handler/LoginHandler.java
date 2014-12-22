b2952366a273a342c895ab2fbccc5f415f955ff9
liguangming <510941969@qq.com>
2014-12-22 19:27:07 +0800

    修改了PAD登录

--- interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/LoginHandler.java
+++ interclass/student_app/AClassRoom/src/cn/com/incito/socket/handler/LoginHandler.java
@@ -41,7 +41,7 @@ public class LoginHandler extends MessageHandler {
 		Student student = data.getObject("student", Student.class);
 		if (student == null) {
 			UIHelper.getInstance().getSelectWifiActivity().showToast();
-		} else {
+		}else{
 			// MultiCastSocket.getInstance().start();//建立广播socket
 			MyApplication.getInstance().setStudent(student);
 			// 判断学生状态跳转至不同的界面
@@ -49,78 +49,14 @@ public class LoginHandler extends MessageHandler {
 			MyApplication.Logger.debug("返回状态值:" + state);
 			if (state == 1) {
 				MyApplication.Logger.debug("返回状态值进入准备上课界面");
-				if (!AppManager.getAppManager().currentActivity().getClass()
-						.getSimpleName().equals("ClassReadyActivity")) {
+				if (!AppManager.getAppManager().currentActivity().getClass().getSimpleName().equals("ClassReadyActivity")) {
 					AppManager.getAppManager().currentActivity().finish();
 					MyApplication.Logger.debug("返回状态值进入准备上课界面成功");
 					UIHelper.getInstance().showClassReadyActivity();
 				}
-
-			} else if (state == 2) {
-				MyApplication.Logger.debug("返回状态值进入分组界面");
-				// 判断该学生是在已经提交的组还是未提交的组还是没有分组
-				List<Group> tempGrou = JSON.parseArray(data.getString("group"),
-						Group.class);
-				List<Group> groupConfirm = JSON.parseArray(
-						data.getString("groupConfirm"), Group.class);
-
-				if (tempGrou != null && tempGrou.size() > 0) {
-					// 判断当前学生是否是未提交小组的成员
-					for (int i = 0; i < tempGrou.size(); i++) {
-						Group group = tempGrou.get(i);
-						if (student.getId() == group.getCaptainId()) {// 如果是组长则进入等待其他小组成员界面
-							MyApplication.getInstance().setGroup(group);
-							UIHelper.getInstance().showConfirmGroupActivity(
-									data.getString("group"));
-
-						} else {// 判断是否是未提交小组成员
-							List<Student> students = group.getStudents();
-							for (int j = 0; j < students.size(); j++) {
-								Student s = students.get(j);
-								if (s.getId() == student.getId()) {
-									MyApplication.getInstance().setGroup(group);
-									UIHelper.getInstance()
-											.showConfirmGroupActivity(
-													data.getString("group"));
-								} else {
-									UIHelper.getInstance().showGroupSelect(
-											data.getString("group"));
-								}
-							}
-						}
-					}
-				} else if (groupConfirm != null && groupConfirm.size() > 0) {
-					// 判断当前学生是否是已经提交的小组成员
-					for (int i = 0; i < groupConfirm.size(); i++) {
-						Group group = groupConfirm.get(i);
-						if (student.getId() == group.getCaptainId()) {
-							MyApplication.getInstance().setGroup(group);
-							UIHelper.getInstance().showClassingActivity();
-						} else {
-							List<Student> students = group.getStudents();
-							for (int j = 0; j < students.size(); j++) {
-								Student s = students.get(j);
-								if (s.getId() == student.getId()) {
-									MyApplication.getInstance().setGroup(group);
-									UIHelper.getInstance()
-											.showClassingActivity();
-								} else {
-									UIHelper.getInstance().showGroupSelect(
-											data.getString("group"));
-								}
-							}
-						}
-					}
-				} else {
-					UIHelper.getInstance().showGroupSelect(
-							data.getString("group"));
-				}
-
-				AppManager.getAppManager().currentActivity().finish();
 			} else if (state == 3) {
 				MyApplication.Logger.debug("返回状态值进入做作业界面");
-				if (!AppManager.getAppManager().currentActivity().getClass()
-						.getSimpleName().equals("DrawBoxActivity")) {
+				if (!AppManager.getAppManager().currentActivity().getClass().getSimpleName().equals("DrawBoxActivity")) {
 					byte[] imageByte;
 					if (isFileExists()) {
 						imageByte = bmpToByteArray(BitmapFactory.decodeFile("/sdcard/temp.jpg"),true);
@@ -128,31 +64,24 @@ public class LoginHandler extends MessageHandler {
 						imageByte = data.getBytes("quiz");
 					}
 					UIHelper.getInstance().showDrawBoxActivity(imageByte);
-					if (!"ClassingActivity".equals(AppManager.getAppManager()
-							.currentActivity().getClass().getSimpleName())) {
+					if (!"ClassingActivity".equals(AppManager.getAppManager().currentActivity().getClass().getSimpleName())) {
 						AppManager.getAppManager().currentActivity().finish();
 					}
 				} else {
 					byte[] imageByte;
 					if (isFileExists()) {
-						UIHelper.getInstance()
-								.getDrawBoxActivity()
-								.setBackGround(BitmapFactory.decodeFile("/sdcard/temp.jpg"));
+						UIHelper.getInstance().getDrawBoxActivity().setBackGround(BitmapFactory.decodeFile("/sdcard/temp.jpg"));
 					} else {
 						// 在原来界面,不做操作
 					}
-
 				}
-
 			} else if (state == 4) {
 				if (isFileExists()) {
 					byte[] imageByte = bmpToByteArray(BitmapFactory.decodeFile("/sdcard/temp.jpg"),true);
 					UIHelper.getInstance().showDrawBoxActivity(imageByte);
 				} else {
 					MyApplication.Logger.debug("返回状态值进入开始上课界面");
-					if (!AppManager.getAppManager().currentActivity()
-							.getClass().getSimpleName()
-							.equals("ClassingActivity")) {
+					if (!AppManager.getAppManager().currentActivity().getClass().getSimpleName().equals("ClassingActivity")) {
 						AppManager.getAppManager().currentActivity().finish();
 						UIHelper.getInstance().showClassingActivity();
 						MyApplication.Logger.debug("返回状态值进入开始上课界面成功");
@@ -162,7 +91,6 @@ public class LoginHandler extends MessageHandler {
 				MyApplication.Logger.debug("没有返回值");
 				AppManager.getAppManager().currentActivity().finish();
 				UIHelper.getInstance().showClassReadyActivity();
-
 			}
 			// 启动心跳检测
 			ConnectionManager.getInstance(message.getChannel());
@@ -191,10 +119,71 @@ public class LoginHandler extends MessageHandler {
 
 	public boolean isFileExists() {
 		File f = new File("/sdcard/", "temp.jpg");
-		if (f.exists()) {
+		if (f.exists()&&f.length()>0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
+//	} else if (state == 2) {
+//	MyApplication.Logger.debug("返回状态值进入分组界面");
+//	// 判断该学生是在已经提交的组还是未提交的组还是没有分组
+//	List<Group> tempGrou = JSON.parseArray(data.getString("group"),
+//			Group.class);
+//	List<Group> groupConfirm = JSON.parseArray(
+//			data.getString("groupConfirm"), Group.class);
+//
+//	if (tempGrou != null && tempGrou.size() > 0) {
+//		// 判断当前学生是否是未提交小组的成员
+//		for (int i = 0; i < tempGrou.size(); i++) {
+//			Group group = tempGrou.get(i);
+//			if (student.getId() == group.getCaptainId()) {// 如果是组长则进入等待其他小组成员界面
+//				MyApplication.getInstance().setGroup(group);
+//				UIHelper.getInstance().showConfirmGroupActivity(
+//						data.getString("group"));
+//			} else {// 判断是否是未提交小组成员
+//				List<Student> students = group.getStudents();
+//				for (int j = 0; j < students.size(); j++) {
+//					Student s = students.get(j);
+//					if (s.getId() == student.getId()) {
+//						MyApplication.getInstance().setGroup(group);
+//						UIHelper.getInstance()
+//								.showConfirmGroupActivity(
+//										data.getString("group"));
+//					} else {
+//						UIHelper.getInstance().showGroupSelect(
+//								data.getString("group"));
+//					}
+//				}
+//			}
+//		}
+//	} else if (groupConfirm != null && groupConfirm.size() > 0) {
+//		// 判断当前学生是否是已经提交的小组成员
+//		for (int i = 0; i < groupConfirm.size(); i++) {
+//			Group group = groupConfirm.get(i);
+//			if (student.getId() == group.getCaptainId()) {
+//				MyApplication.getInstance().setGroup(group);
+//				UIHelper.getInstance().showClassingActivity();
+//			} else {
+//				List<Student> students = group.getStudents();
+//				for (int j = 0; j < students.size(); j++) {
+//					Student s = students.get(j);
+//					if (s.getId() == student.getId()) {
+//						MyApplication.getInstance().setGroup(group);
+//						UIHelper.getInstance()
+//								.showClassingActivity();
+//					} else {
+//						UIHelper.getInstance().showGroupSelect(
+//								data.getString("group"));
+//					}
+//				}
+//			}
+//		}
+//	} else {
+//		UIHelper.getInstance().showGroupSelect(
+//				data.getString("group"));
+//	}
+//	AppManager.getAppManager().currentActivity().finish();
+	
+	
 }
