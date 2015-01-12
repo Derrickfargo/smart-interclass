4c8b1fd9390744fbfec3b4abd3096a539cbaef98
1977979333@qq.com <619822hm>
2015-01-12 14:36:11 +0800

    修改代码位置，将停止连接代码写错了位置

--- dreamclass/student_app/AClassRoom/src/cn/com/incito/socket/core/NMainHandler.java
+++ dreamclass/student_app/AClassRoom/src/cn/com/incito/socket/core/NMainHandler.java
@@ -36,15 +36,15 @@ public class NMainHandler extends ChannelInboundHandlerAdapter {
 		MyApplication.getInstance().setChannelHandlerContext(ctx);
 		MyApplication.Logger.debug(AndroidUtil.getCurrentTime() + ":NMainHandler:连接上服务器");
 		
-		NCoreSocket.getInstance().stopConnection();
-		//停顿3s后进行重连连接
-		Thread.sleep(3000);
-		NCoreSocket.getInstance().startConnection(Constants.IP, Constants.PORT);
 		
 	}
 
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
 		MyApplication.getInstance().setChannelHandlerContext(null);
+		NCoreSocket.getInstance().stopConnection();
+		//停顿3s后进行重连连接
+		Thread.sleep(3000);
+		NCoreSocket.getInstance().startConnection(Constants.IP, Constants.PORT);
 		MyApplication.Logger.debug(AndroidUtil.getCurrentTime()+ ":NMainHandler:断开连接");
 	}
 
